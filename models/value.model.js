8f0eed572569660abcf69fbbb7932b4964e733a0
Saimon Alam <ibnshafique@gmail.com>
2017-08-26 04:54:26 +0600

    changed expiry time to 5 minutes

--- models/value.model.js
+++ models/value.model.js
@@ -3,7 +3,7 @@ var Schema=mongoose.Schema;
 
 
 var ValueSchema=new Schema({
-    expire_at: {type: Date, default: Date.now, expires: 7200} 
+    expire_at: {type: Date, default: Date.now, expires: 300} 
 },{ strict: false });
 
 module.exports=mongoose.model('Value',ValueSchema);
